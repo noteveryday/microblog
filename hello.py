271d88636eb8617f69282abe3a1b8365212753e0
noteveryday <bzflyg@gmail.com>
2016-05-24 15:56:01 +0800

    hello

--- hello.py
+++ hello.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import request
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    host = request.headers.get('Host')
+    return '<h1> Hello World!</h1><br /><p>Your browser is %s.<br/>Your Host is %s.</p>' % (user_agent,host)
 
 if __name__ == '__main__':
     app.run(debug = True)
