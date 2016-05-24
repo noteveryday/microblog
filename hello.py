233518ad11bf892d037ef486a2606887e9a03b50
noteveryday <bzflyg@gmail.com>
2016-05-24 16:02:27 +0800

    400

--- hello.py
+++ hello.py
@@ -4,10 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    host = request.headers.get('Host')
-    return '<h1> Hello World!</h1><br /><p>Your browser is %s.<br/>Your Host is %s.</p>' % (user_agent,host)
-
+    return '<h1> Bad Request</h1>' 400
 if __name__ == '__main__':
     app.run(debug = True)
 # test
