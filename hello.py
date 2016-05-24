48265c65a5c8e4a870067fa815ae386e308ea743
noteveryday <bzflyg@gmail.com>
2016-05-24 16:18:07 +0800

    response

--- hello.py
+++ hello.py
@@ -1,9 +1,17 @@
 from flask import Flask
-from flask import request
+from flask import make_response, abort
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> Bad Request</h1>' 400
+    response = make_response('<h1>This document carries a cookie!</h1>')
+    response.set_cookie('answer', '42')
+    return response
+@app.route('/user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return '<h1> hello, %s' % user.name
 if __name__ == '__main__':
     app.run(debug = True)
