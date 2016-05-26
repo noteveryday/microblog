22b81c0294def7fa02c7806379768dd8da619abc
noteveryday <bzflyg@gmail.com>
2016-05-26 11:48:14 +0800

    bootstrap

--- hello.py
+++ hello.py
@@ -2,18 +2,33 @@ from flask import Flask, render_template
 from flask import make_response, abort
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'You gueess not'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time = datetime.utcnow())
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
 
+# if __name__ == '__main__':
+#     app.run(debug=True)
